aa3e9fa3027bd97b5609af1ac4ca1ad82eaf1b08
Didzis Bondars <your@email.comdidz.bondars@gmail.com>
2026-02-19 16:43:26 +0200

    Fix: add missing mapbox-gl CSS import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
